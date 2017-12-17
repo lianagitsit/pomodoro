5d73358916a998e782ae9e8d2fee0e1bd416954d
Liana Mancini <lianamancini@Lianas-MacBook-Pro.local>
2017-12-17 16:40:22 -0500

    oh god what happened fix this

--- main.js
+++ main.js
@@ -13,54 +13,68 @@ $(document).ready(function () {
     var workInput = document.getElementById("workInput");
     var breakInput = document.getElementById("breakInput");
     var message = document.getElementById("message");
+
+    var reset = document.getElementById("reset");
+    var resetClicked = false;
+
     var count;
     var timerIsOn = false;
 
     const WORK_DEFAULT = 1;
     const BREAK_DEFAULT = 1;
 
-    // create sound object
-    //var timesUpSound = new Audio("wind-chime-2.mp3");
-
     //set default time
     workInput.value = WORK_DEFAULT;
     breakInput.value = BREAK_DEFAULT;
-    //timerDisplay.textContent = WORK_DEFAULT;
-
-    // countdown from defaults
-    // when user clicks timer display, start countdown
+  
+    // when user clicks START, start clock
     timerDisplay.addEventListener("click", startClock);
 
+    // when user clicks RESET, start clock from defaults
+    reset.addEventListener("click", clickReset);
+
+    function clickReset(){
+        timerIsOn = false;
+    }
+
     function startClock(){
-        var minutes;
-        var seconds;
+        console.log("start clicked");
+        var minutes = workInput.value;
+        var seconds = 0;
         var itsWorkTime = true;
         var myInterval = 500;
-
+        
         timerIsOn = true;
+
         if (timerIsOn){
             timerDisplay.removeEventListener("click", startClock);
-            //timerDisplay.addEventListener("click", pauseClock);
+            //reset.removeEventListener("click", startClock);
+            timerDisplay.addEventListener("click", pauseClock);
         }
-        
-        minutes = workInput.value;
-        seconds = 0;
 
-        // fires the countdown function every second
+        // fires the countdown function at interval
         setTimeout(countdown, myInterval);
 
         function countdown() {
 
+            // check for pause condition
+            if (!timerIsOn){
+                return;
+            }
+            
             if (itsWorkTime){
-                message.innerHTML = "Work :-(";
+               $(breakInput).removeClass("green");
+               $(workInput).addClass("red");
             } else {
-                message.innerHTML = "Break :-)";
-            }    
+                $(workInput).removeClass("red");
+                $(breakInput).addClass("green");
+            }
 
-            // when one timer finishes, roll to the next
             if (minutes === 0 && seconds === 0) {
+                // play chime at end of current timer
                 document.getElementById("sound").play();
-                //timesUpSound.play();
+
+                // when one timer finishes, set timer to the next
                 if (itsWorkTime){
                     minutes = breakInput.value;
                     itsWorkTime = false;    
@@ -85,26 +99,23 @@ $(document).ready(function () {
                 seconds = seconds - 1;
             }        
 
-            setTimeout(countdown, myInterval);
+            setTimeout(countdown, myInterval);           
             
-            //console.log("Time left: " + minutes + "minutes and " + seconds + " seconds.");
         }
 
+        function pauseClock(){
+            timerIsOn = false;
+            //timerDisplay.removeEventListener("click", pauseClock);
+            //timerDisplay.addEventListener("click", resumeClock);
+            //reset.addEventListener("click", startClock);
+        }
+
+        function resumeClock(){
+            timerIsOn = true;
+            //timerDisplay.removeEventListener("click", resumeClock);
+            //timerDisplay.addEventListener("click", pauseClock);
+            countdown();
+        }
     }
-
-
-       /* // listens for user entry into input field and fires timer setting
-    workInput.addEventListener("input", myFunc);
-
-    function myFunc(event) {
-        // console.log(event);
-        console.log("input received: " + event.target.value);
-
-        // sets the empty timer space to user's input
-        // convert minutes to seconds
-        // change this to more secure method
-        timerDisplay.innerHTML = workInput.value;
-    }
-    */
 
 });
