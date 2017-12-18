d1fdf9288acc1ea4b25344c9eb1724ea6248be9e
Liana Mancini <lianamancini@Lianas-MacBook-Pro.local>
2017-12-17 22:00:09 -0500

    added input validation

--- main.js
+++ main.js
@@ -9,92 +9,118 @@
 
 $(document).ready(function () {
 
+    // DOM elements
     var timerDisplay = document.getElementById("timerDisplay");
     var workInput = document.getElementById("workInput");
     var breakInput = document.getElementById("breakInput");
     var message = document.getElementById("message");
-
     var reset = document.getElementById("reset");
-    var resetClicked = false;
 
-    var count = 0;
-    var timerIsOn = false;
+    // state variables
+    var count;
+    var timerIsOn;
+    var itsWorkTime;
+    var inputIsValid = true;
 
-    const WORK_DEFAULT = 1;
-    const BREAK_DEFAULT = 1;
+    // clock runtime in milliseconds
+    var myInterval = 50;
 
-    //set default time
-    workInput.value = WORK_DEFAULT;
-    breakInput.value = BREAK_DEFAULT;
+    // default session time values
+    const WORK_DEFAULT = 25;
+    const BREAK_DEFAULT = 5;
+
+    // set clock at start
+    var minutes;
+    var seconds;
+    
+    // validate that user input is a positive integer
+    function validateInput(){
+        var inputs = [workInput, breakInput];
+        for (var i = 0; i < inputs.length; i++){
+            inputs[i].addEventListener("input", function(event){
+                //console.log(`This is ${this} and its value is ${this.value}`);
+                if (workInput.value <= 0 || workInput.value % 1 !== 0 || breakInput.value <= 0 || breakInput.value % 1 !== 0 ){
+                    message.innerHTML = "time must be in whole minutes!";
+                    inputIsValid = false;
+                } else {
+                    message.innerHTML = "";
+                    inputIsValid = true;
+                }
+            })
+        }
+    }
   
-    // when user clicks START, start clock
+    // timer click event
     timerDisplay.addEventListener("click", handleTimerClick);
 
-    // when user clicks RESET, start clock from defaults
-    //reset.addEventListener("click", clickReset);
-
-    /*function clickReset(){
-        timerIsOn = false;
-    }*/
-
-    var myInterval = 500;
-
     function handleTimerClick(){
-        // if the timer isn't on and it's the first session, start the clock
-        if (!timerIsOn && count === 0){
-            timerIsOn = true;
-            countdown();
-        // if the clock is running, pause
-        } else if (timerIsOn){
-            timerIsOn = false;
-        } else if (!timerIsOn && count > 0){
-            timerIsOn = true;
-            countdown();
+        // disable 
+        if (!inputIsValid){
+            return;
+        } else {
+        
+            // if it's the first session, start the clock
+            if (!timerIsOn && count === 0){
+                timerIsOn = true;
+
+                // disable input fields until clock is reset
+                breakInput.disabled = true;
+                workInput.disabled = true;
+
+                // show reset button
+                reset.style.visibility = "visible";
+
+                // set minutes to work input
+                minutes = workInput.value;
+
+                countdown();
+            // if the clock is running, pause
+            } else if (timerIsOn){
+                timerIsOn = false;
+            // if the clock is paused, resume
+            } else if (!timerIsOn && count > 0){
+                timerIsOn = true;
+                breakInput.disabled = true;
+                workInput.disabled = true;
+                countdown();
+            }
         }
     }
 
-    /*function startClock(){
-        var minutes = workInput.value;
-        var seconds = 0;
-        
-        timerIsOn = true;
+    // on reset, stop countdown and set clock back to default values
+    reset.addEventListener("click", setDefaults);
 
-        /*if (timerIsOn){
-            //timerDisplay.removeEventListener("click", startClock);
-            //reset.removeEventListener("click", startClock);
-            timerDisplay.addEventListener("click", pauseClock);
-        }*/
+    function setDefaults(){
+        count = 0;
+        timerIsOn = false;
+        itsWorkTime = true;
 
-        // fires the countdown function at interval
-        //setTimeout(countdown, myInterval);
+        workInput.value = WORK_DEFAULT;
+        breakInput.value = BREAK_DEFAULT;
 
+        minutes = workInput.value;
+        seconds = 0;
 
-        /*function pauseClock(){
-            timerIsOn = false;
-            //timerDisplay.removeEventListener("click", pauseClock);
-            //timerDisplay.addEventListener("click", resumeClock);
-            //reset.addEventListener("click", startClock);
-        }
+        validateInput();
 
-        function resumeClock(){
-            timerIsOn = true;
-            //timerDisplay.removeEventListener("click", resumeClock);
-            //timerDisplay.addEventListener("click", pauseClock);
-            countdown();
-        }
-    }*/
+        breakInput.disabled = false;
+        workInput.disabled = false;
 
-    var minutes = workInput.value;
-    var seconds = 0;
-    var itsWorkTime = true;
+        timerDisplay.innerHTML = "start";
+        reset.style.visibility = "hidden";
+
+        $(breakInput).removeClass("green");
+        $(workInput).removeClass("red");
+    }
+
 
     function countdown() {
-
         // check for pause condition
         if (!timerIsOn){
             return;
-        }
+        } 
 
+        // toggle work/break border color
         if (itsWorkTime){
            $(breakInput).removeClass("green");
            $(workInput).addClass("red");
@@ -132,11 +158,13 @@ $(document).ready(function () {
             seconds = seconds - 1;
         }   
         
+        // increment for state
         count += 1;
-        console.log("count: " + count);
 
         setTimeout(countdown, myInterval);           
         
     }
+
+    setDefaults();
 
 });
